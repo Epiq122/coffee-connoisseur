4d11f6d40396b794f6e3660489fe6c348632672f
Robert Gleason <l2ob122@gmail.com>
2023-01-18 08:55:21 -0800

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import '@/styles/globals.css';
+import { Roboto } from '@next/font/google;';
+
+const roboto = Roboto({
+  weights: [400, 500, 700],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <main className={roboto.className}>
       <Component {...pageProps} />
-    </div>
+    </main>
   );
 }
